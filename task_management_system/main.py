a6c72c1db9f618cda3038335629272208d2a2a34
Harsh Mendhe <harshmendhe456@gmail.com>
2024-01-31 16:07:49 +0530

    first commit

---
+++ task_management_system/main.py
@@ -0,0 +1,15 @@
+from task_package.create_task import create_task
+from task_package.edit_task import edit_task
+from task_package.categorize_task import categorize_task
+#create a task
+task1=create_task('Complete Your Assignment','Finish TASK MANAGEMENT SYSTEM')
+#display the task
+print('Task 1:',task1)
+#edit the task
+edit_task(task1,new_title='Updated Assignment',new_description='Review and Submit to Rashmi Mam')
+#display the updated task
+print('Updated task 1:',task1)
+#categorize the task
+categorize_task(task1,'Zappcode Academy')
+#display the categorized task
+print('Categorized Task 1:',task1)
